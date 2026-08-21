46852ecd3011183b6b7d680a78fd6c351ac49a69
Jennifer Wilcox <jennifer@nitori.org>
2026-08-20 19:28:40 -0500

    Initial board bootstrap

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::gpio;
+use embassy_time::Timer;
+use gpio::{Level, Output};
+use panic_probe as _;
+
+// Program metadata for `picotool info`.
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"j2calc"),
+    embassy_rp::binary_info::rp_program_description!(c"Programmer's calculator"),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+#[embassy_executor::main(
+    executor = "embassy_rp::executor::Executor",
+    entry = "cortex_m_rt::entry"
+)]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_7, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
+}
