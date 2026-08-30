83b87b9ab3f334e4c1e837c43e90e71bb3077e6e
Jennifer Wilcox <jennifer@nitori.org>
2026-08-30 11:18:47 -0500

    Gettin silly with it

--- src/lcd.rs
+++ src/lcd.rs
@@ -1,14 +1,10 @@
 use embedded_graphics::{
-    Drawable, Pixel,
-    geometry::{Dimensions, Point, Size},
-    mono_font::{MonoTextStyle, ascii::FONT_6X10},
-    pixelcolor::BinaryColor,
-    primitives::Rectangle,
-    text::{Alignment, Text},
+    Drawable, Pixel, geometry::{Dimensions, Point, Size}, image::{Image, ImageRaw}, mono_font::{MonoTextStyle, ascii::FONT_10X20}, pixelcolor::BinaryColor, primitives::Rectangle, text::{Alignment, Text}
 };
 use embedded_graphics_core::draw_target::DrawTarget;
 use embedded_hal_1::digital::OutputPin;
 use embedded_hal_async::spi::SpiBus;
+use embassy_time::Timer;
 
 const WIDTH: usize = 400;
 const HEIGHT: usize = 240;
@@ -43,6 +39,7 @@ impl<SPI: SpiBus<u8>, CS: OutputPin> Lcd<SPI, CS> {
 
     pub async fn update_vcom(&mut self) -> Result<(), Error> {
         self.cs.set_high().map_err(|_| Error::Gpio)?;
+        Timer::after_micros(1).await;
 
         let cmd = [self.toggle_vcom(), 0u8];
         self.spi.write(&cmd).await.map_err(|_| Error::Spi)?;
@@ -55,6 +52,7 @@ impl<SPI: SpiBus<u8>, CS: OutputPin> Lcd<SPI, CS> {
 
     pub async fn clear_screen(&mut self) -> Result<(), Error> {
         self.cs.set_high().map_err(|_| Error::Gpio)?;
+        Timer::after_micros(1).await;
 
         let cmd = [self.toggle_vcom() | CLEAR_BIT, 0u8];
         self.spi.write(&cmd).await.map_err(|_| Error::Spi)?;
@@ -66,6 +64,7 @@ impl<SPI: SpiBus<u8>, CS: OutputPin> Lcd<SPI, CS> {
 
     pub async fn flush(&mut self) -> Result<(), Error> {
         self.cs.set_high().map_err(|_| Error::Gpio)?;
+        Timer::after_micros(1).await;
 
         // TODO: Be smarter and only update lines that changed, could save power
         let cmd = [self.toggle_vcom() | WRITE_BIT, 0u8];
@@ -125,18 +124,43 @@ impl<SPI: SpiBus<u8>, CS: OutputPin> DrawTarget for Lcd<SPI, CS> {
             .for_each(|p| self.set_pixel(p.0.x, p.0.y, p.1));
         Ok(())
     }
+
+    fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
+        let value = if color == BinaryColor::Off { 0xFF } else {0};
+        self.framebuffer = [[value; WIDTH/8]; HEIGHT];
+        Ok(())
+    }
 }
 
-pub fn draw_splash<D: DrawTarget<Color = BinaryColor>>(display: &mut D) -> Result<(), D::Error> {
+const INTRO: &[&str] = textwrap_macros::wrap!("Did you ever hear the tragedy of Darth Plagueis the Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.", 40);
+const IMAGE_DATA: &[u8] = include_bytes!("../Darth.bin");
+pub fn draw_splash<D: DrawTarget<Color = BinaryColor>>(
+    display: &mut D,
+    y_offset: i32,
+) -> Result<(), D::Error> {
     display.clear(BinaryColor::Off)?;
-    let character_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
-    Text::with_alignment(
-        "j2calc",
-        display.bounding_box().center(),
-        character_style,
-        Alignment::Center,
-    )
-    .draw(display)?;
+
+
+    let raw_image = ImageRaw::<BinaryColor>::new(IMAGE_DATA, 400);
+
+    let image = Image::new(&raw_image, Point::new(0, y_offset));
+    image.draw(display)?;
+
+
+    let character_style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+    // Text::with_alignment(
+    //     "j2calc",
+    //     Point::new((WIDTH / 2) as i32, y_offset),
+    //     character_style,
+    //     Alignment::Center,
+    // )
+    // .draw(display)?;
+
+    let mut line_y = y_offset + 240;
+    for line in INTRO {
+        Text::with_alignment(line, Point::new(0, line_y), character_style, Alignment::Left).draw(display)?;
+        line_y += 20;
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use embassy_rp::{
     bind_interrupts, gpio, peripherals,
     spi::{Config, Phase, Polarity, Spi},
 };
-use embassy_time::Timer;
+use embassy_time::{Duration, Instant, Timer};
 use gpio::{Level, Output};
 use panic_probe as _;
 
 mod lcd;
+
+const DISPLAY_REFRESH_PERIOD: Duration = Duration::from_hz(8);
 
 // Program metadata for `picotool info`.
 #[unsafe(link_section = ".bi_entries")]
@@ -28,6 +30,14 @@ bind_interrupts!(struct Irqs {
     DMA_IRQ_0 => embassy_rp::dma::InterruptHandler<peripherals::DMA_CH0>;
 });
 
+async fn measure_duration(name: &str, f: impl AsyncFnOnce() -> ()) {
+    let start = embassy_time::Instant::now();
+    f().await;
+    let end = embassy_time::Instant::now();
+
+    info!("{} took {} microseconds", name, end.duration_since(start).as_micros());
+}
+
 #[embassy_executor::main(
     executor = "embassy_rp::executor::Executor",
     entry = "cortex_m_rt::entry"
@@ -37,26 +47,46 @@ async fn main(_spawner: Spawner) {
     let mut led = Output::new(p.PIN_7, Level::Low);
 
     let mut lcd_spi_config = Config::default();
-    lcd_spi_config.frequency = 1_000_000;
+    lcd_spi_config.frequency = 2_000_000;
     lcd_spi_config.phase = Phase::CaptureOnSecondTransition;
     lcd_spi_config.polarity = Polarity::IdleLow;
     let lcd_spi = Spi::new_txonly(p.SPI0, p.PIN_22, p.PIN_23, p.DMA_CH0, Irqs, lcd_spi_config);
     let mut lcd = lcd::Lcd::new(lcd_spi, Output::new(p.PIN_25, Level::Low));
 
     lcd.clear_screen().await.unwrap();
-    lcd::draw_splash(&mut lcd);
     lcd.flush().await.unwrap();
 
+    let mut y_offset = 240;
+    let mut next_led_toggle = Instant::now();
+
     loop {
-        info!("led on!");
-        led.set_high();
-        Timer::after_millis(500).await;
+        let now = Instant::now();
+        let end_of_frame = now + DISPLAY_REFRESH_PERIOD;
 
-        info!("led off!");
-        led.set_low();
-        Timer::after_millis(500).await;
+        if now > next_led_toggle {
+            led.set_low();
+        }
 
-        // TODO: Move to separate task
-        lcd.update_vcom().await.unwrap();
+        y_offset -= 2;
+        if y_offset < -240 * 6 {
+            y_offset = 0;
+        }
+        lcd::draw_splash(&mut lcd, y_offset).unwrap();
+
+        lcd.flush().await.unwrap();
+
+        let now = Instant::now();
+        if now > end_of_frame {
+            warn!("Missed timing by {} us", (now - end_of_frame).as_micros());
+            led.set_high();
+            next_led_toggle = now + Duration::from_secs(1);
+        }
+
+        Timer::at(end_of_frame).await;
     }
+}
+
+#[embassy_executor::task]
+async fn screen_updater() {
+    
 }
